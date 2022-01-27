007e6c5854e3cb87ed16d873710a015427386e3a
Antonio-Cesaria <s290261@studenti.polito.it>
2022-01-27 14:59:20 +0100

    multiple games working

--- agent2.py
+++ agent2.py
@@ -1,4 +1,5 @@
 from os import stat
+import os
 import socket
 from sys import stdout
 from threading import Thread
@@ -97,67 +98,92 @@ def commandShow(playerName: str, s: socket):  # SHOW (just first time)
     # print(type(data))
     if type(data) is GameData.ServerGameStateData:
         dataOk = True
-        print("Current player: " + data.currentPlayer)
-        print("Player hands: ")
-        for p in data.players:
-            print(p.toClientString())
-            # SAVING INFORMATIONS => cards[0] avremo la hand di player0
-        print("Cards in your hand: " + str(data.handSize))
-        print("Table cards: ")
-        for pos in data.tableCards:
-            print(pos + ": [ ")
-            for c in data.tableCards[pos]:
-                print(c.toClientString() + " ")
-                print("]")
-        print("Discard pile: ")
-        for c in data.discardPile:
-            print("\t" + c.toClientString())
-        print("Note tokens used: " + str(data.usedNoteTokens) + "/8")
-        print("Storm tokens used: " + str(data.usedStormTokens) + "/3")
+        # Removing print for debugging purpose (too much print...)
+
+        # print("Current player: " + data.currentPlayer)
+        # print("Player hands: ")
+        # for p in data.players:
+        #     print(p.toClientString())
+        #     # SAVING INFORMATIONS => cards[0] avremo la hand di player0
+        # print("Cards in your hand: " + str(data.handSize))
+        # print("Table cards: ")
+        # for pos in data.tableCards:
+        #     print(pos + ": [ ")
+        #     for c in data.tableCards[pos]:
+        #         print(c.toClientString() + " ")
+        #         print("]")
+        # print("Discard pile: ")
+        # for c in data.discardPile:
+        #     print("\t" + c.toClientString())
+        # print("Note tokens used: " + str(data.usedNoteTokens) + "/8")
+        # print("Storm tokens used: " + str(data.usedStormTokens) + "/3")
+        return data
+    return None
 
 
-# currently not properly working...
-def commandPlay(cardPos: int, playerName: str, s: socket):
-    global run
-    s.send(GameData.ClientPlayerPlayCardRequest(
-        playerName, cardPos).serialize())
-    data = s.recv(DATASIZE)
+def manageHintResponse(data):
     data = GameData.GameData.deserialize(data)
+    if type(data) is not GameData.ServerHintData:
+        print("Invalid action, terminating", type(data))
+        os._exit(-1)
 
+    print("Hint type: " + data.type)
+    print("Player " + data.destination +
+          " cards with value " + str(data.value) + " are:")
+    for i in data.positions:
+        print("\t" + str(i))
+
+
+def managePlayResponse(data):
+    data = GameData.GameData.deserialize(data)
     if type(data) is GameData.ServerPlayerMoveOk:
-        return (1, None)
-
+        print("Nice move!")
+        print("Current player: " + data.player)
+        return 1
     if type(data) is GameData.ServerPlayerThunderStrike:
-        return (0, None)
-
+        print("OH NO! The Gods are unhappy with you!")
+        return -1
     if type(data) is GameData.ServerGameOver:
-        stdout.flush()
-        return -1, data.score
         print(data.message)
         print(data.score)
         print(data.scoreMessage)
-    if type(data) is GameData.ServerGameStateData:
-        dataOk = True
-        print("Current player: " + data.currentPlayer)
-        print("Player hands: ")
-        for p in data.players:
-            print(p.toClientString())
-        print("Cards in your hand: " + str(data.handSize))
-        print("Table cards: ")
-        for pos in data.tableCards:
-            print(pos + ": [ ")
-        for c in data.tableCards[pos]:
-            print(c.toClientString() + " ")
-            print("]")
-            print("Discard pile: ")
-            for c in data.discardPile:
-                print("\t" + c.toClientString())
-            print("Note tokens used: " + str(data.usedNoteTokens) + "/8")
-            print("Storm tokens used: " + str(data.usedStormTokens) + "/3")
+        stdout.flush()
+        print("Ready for a new game!")
+        return 0
+
+# Function that updates the HintTable after an Hint
 
 
+def manageHintTableHintUpdate(data):
+    global numPlayers
+    global hintTable
+    data = GameData.GameData.deserialize(data)
+    for i in range(getNumSlots(numPlayers)):
+        if i in data.positions:
+            if data.type == "color":
+                hintTable[int(data.destination[-1:])
+                          ][i].directHintColor(data.value)
+            elif data.type == "value":
+                hintTable[int(data.destination[-1:])
+                          ][i].directHintValue(data.value)
+            else:
+                print("ERROR: Wrong hint type")
+        else:
+            if data.type == "color":
+                hintTable[int(data.destination[-1:])
+                          ][i].undirectHintColor(data.value)
+            elif data.type == "value":
+                hintTable[int(data.destination[-1:])
+                          ][i].undirectHintValue(data.value)
+            else:
+                print("ERROR: Wrong hint type")
 
-    return None
+# Function that updates the HintTable after a play or a discard
+
+
+def manageHintTableUpdate(data):
+    pass
+
 
 run = True
 
@@ -173,22 +199,34 @@ plays = [0]  # just to debug
 
 cards = []
 
-hintTable = [[0 for x in range(getNumSlots(numPlayers))] for y in range(numPlayers)] #Array of shape H=[#Players][#Slots]
+hintTable = [[0 for x in range(getNumSlots(numPlayers))] for y in range(
+    numPlayers)]  # Array of shape H=[#Players][#Slots]
+
 
 def playIfCertain(playerNum: int, hintTable):
-    for slot in range(getNumSlots(numPlayers)):    #ok to import them? is there a better way?        
+    # ok to import them? is there a better way?
+    for slot in range(getNumSlots(numPlayers)):
 
-        if(any(el==1 for el in hintTable[slot].values.values()) 
-            & any(el==1 for el in hintTable[slot].colors.values())):
-                print(f"Found playable card for slot number {slot}:")
-                return slot                                         #In this case, we are playing the first playable card
-                                                                    #of the player, there maybe more than one, we can make
-                                                                    #an array and then by some metric (or random) choose one
+        if(any(el == 1 for el in hintTable[slot].values.values())
+                & any(el == 1 for el in hintTable[slot].colors.values())):
+            print(f"Found playable card for slot number {slot}:")
+            return slot  # In this case, we are playing the first playable card
+            # of the player, there maybe more than one, we can make
+            # an array and then by some metric (or random) choose one
+
+
+def hintTableInit():
+    global numPlayers
+    global hintTable
+    for p in range(numPlayers):
+        for slot in range(getNumSlots(numPlayers)):
+            hintTable[p][slot] = CardHints(slots)
 
 
 def main():
     global status
     global numPlayers
+    global hintTable
     print("start simulation")
     clientSockets = []
 
@@ -219,31 +257,34 @@ def main():
         print()
 
     # init card_infos => !!! to update on hint or play or discard
-    for p in range(len(players)):
-        for slot in range(getNumSlots(numPlayers)):
-            hintTable[p][slot] = CardHints(slots)
+    hintTableInit()
 
     print("HINT TABLE player0:")
     for slot in range(getNumSlots(numPlayers)):
         print(hintTable[0][slot].values)
         print(hintTable[0][slot].colors)
 
+    time.sleep(0)
     # init table_cards = {}
     # init scarti = {}
 
     # START GAME
-
+    it = 0
     while run:
         for client in range(numPlayers):
             playerName = "player" + str(client)
+
             s = clientSockets[client]
 
-            print(f"\nfor cycle of player{str(client)}\n")
+            print(f"\nfor cycle of player{client}\n")
 
-            #commandShow("player0", s)
+            # Before starting, every player is doing show in order to update infos
+            game = commandShow(playerName, s)
+            players = game.players
 
-            # 1. think a move
-            move = "hint"
+            # 1. think a move (All players hinting if possible,  but player1)
+            move = "hint" if (
+                playerName != "player1" and game.usedNoteTokens < 8) else "play"
 
             # 2. take action
 
@@ -253,128 +294,82 @@ def main():
                 s.send(GameData.ClientPlayerPlayCardRequest(
                     playerName, cardPos).serialize())
                 data = s.recv(DATASIZE)
-                data = GameData.GameData.deserialize(data)
-                if type(data) is GameData.ServerPlayerMoveOk:
-                    dataOk = True
-                    print("Nice move!")
-                    print("Current player: " + data.player)
-                if type(data) is GameData.ServerPlayerThunderStrike:
-                    dataOk = True
-                    print("OH NO! The Gods are unhappy with you!")
-                if type(data) is GameData.ServerGameOver:
-                    dataOk = True
-                    print(data.message)
-                    print(data.score)
-                    print(data.scoreMessage)
-                    stdout.flush()
-                    run = False
-                    print("Ready for a new game!")
+                res = managePlayResponse(data)
+
+                # this is just ack for other players...
+                for c in range(numPlayers):
+                    if c == client:
+                        continue
+                    data = clientSockets[c].recv(DATASIZE)
+                    res = managePlayResponse(data)
+                # This means game ended, so.. restart
+                if res == 0:
+                    hintTableInit()
                     break
+                # without this multiple occurrencies will happen because slot will change
+                # hintTableUpdate()
+
             elif move == "hint":
-                # (GIVE HINT) 
+                # (GIVE HINT)
                 # 1. send a request
 
                 cardPos = 0
-                typ = "color"
+                typ = "value"
                 dest = "player1"
 
                 # value = ? => Find value of the first card of player1
                 value = players[1].hand[0].value if typ == "value" else players[1].hand[0].color
-                
-                # Debug purpose
+                print(value)
+                time.sleep(2)
+
+                # Debug purpose, just checking that player1 is not hinting (pretty useless..)
                 if playerName == "player1":
+                    print("There must be an error")
+                    time.sleep(10)
                     continue
 
-                
                 # Source Hint
                 s.send(GameData.ClientHintData(
                     playerName, dest, typ, value).serialize())
 
+                # Response must be read first from the sender, then from the others
                 data = s.recv(DATASIZE)
-                data = GameData.GameData.deserialize(data)
+                manageHintResponse(data)
 
-                if type(data) is not GameData.ServerHintData:
-                    print("Invalid action, terminating", type(data))
-                    return
-                print("Hint type: " + data.type)
-                print("Player " + data.destination +
-                      " cards with value " + str(data.value) + " are:")
-                for i in data.positions:
-                    print("\t" + str(i))
-                
-                for i in range(getNumSlots(numPlayers)):
-                    if i in data.positions:
-                        if data.type == "color":
-                            hintTable[int(data.destination[-1:])][i].directHintColor(data.value)
-                        elif data.type == "value":
-                            hintTable[int(data.destination[-1:])][i].directHintValue(data.value)
-                        else:
-                            print("ERROR: Wrong hint type")
-                    else:
-                        if data.type == "color":
-                            hintTable[int(data.destination[-1:])][i].undirectHintColor(data.value)
-                        elif data.type == "value":
-                            hintTable[int(data.destination[-1:])][i].undirectHintValue(data.value)
-                        else:
-                            print("ERROR: Wrong hint type")
+                # HintResponse ACK for the remaining clients
+                for c in range(numPlayers):
+                    if c == client:
+                        continue
+                    data = clientSockets[c].recv(DATASIZE)
+                    manageHintResponse(data)
 
-                #hard code moves, just to test the rule , it might give problems when there are teo correct colors for card 0
-                hintTable[1][0].directHintColor('red')
-                hintTable[1][0].directHintValue(0)
+                # Hint table update
+                manageHintTableHintUpdate(data)
 
-                #Just for testing
+                # hard code moves, just to test the rule , it might give problems when there are teo correct colors for card 0
+                # hintTable[1][0].directHintColor('red')
+                # hintTable[1][0].directHintValue(0)
+
+                # Just for testing
                 print("\nHINT TABLE (after update):")
                 for slot in range(getNumSlots(numPlayers)):
-                    print(f"values: {hintTable[int(data.destination[-1:])][slot].values}")
-                    print(f"colors: {hintTable[int(data.destination[-1:])][slot].colors}\n")
+                    print(
+                        f"values: {hintTable[1][slot].values}")
+                    print(
+                        f"colors: {hintTable[1][slot].colors}\n")
 
-                playableCard = playIfCertain(1, hintTable[1])
-                print(f"\n the playable card is: {playableCard}")
+                # playableCard = playIfCertain(1, hintTable[1])
+                # print(f"\n the playable card is: {playableCard}")
 
-                # Dest Hint (Other data is sent from server) ///// Could we not print it?
-                if playerName == "player0":
-                    s = clientSockets[1]
-                    data = s.recv(DATASIZE)
-                    data = GameData.GameData.deserialize(data)
+                # 2. Put hint info in HINT TABLE (done)
+                # 3. Check if token is correctly decreasing (done)
+                # 4. Remember to update info after play or discard (to do)
 
-                    if type(data) is not GameData.ServerHintData:
-                        return
-                    print("Hint type: " + data.type)
-                    print("Player " + data.destination +
-                          " cards with value " + str(data.value) + " are:")
-                    for i in data.positions:
-                        print("\t" + str(i))
-                
-                #2. Put hint info in HINT TABLE (done)
-                #3. Check if token is correctly decreasing (done)
-                #4. Remember to update info after play or discard (to do) 
+            # elif move == "discard":
+                # Yet to implement. We have to be aware of many things, e.g. when getting the new card, updating the hint table.
 
-
-                time.sleep(3)
-
-            #elif move == "discard":
-                #Yet to implement. We have to be aware of many things, e.g. when getting the new card, updating the hint table.
-            
-
-        time.sleep(1)
+        time.sleep(3)
 
 
 if (__name__ == "__main__"):
     main()
-
-
-# That's how commandPlay could be used
-# res = commandPlay(cardPos, playerName, s)
-
-    # score = res[0]
-    # res = res[0]
-    # print(res)
-    # if res == 0:
-    #     print("Nice move!")
-    # elif res == 1:
-    #     print("OH NO! The Gods are unhappy with you!")
-    # else:
-    #     run = False
-    #     print("score: ", score)
-    #     print("Ready for a new game!")
-    #     break
